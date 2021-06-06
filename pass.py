7ad9213d6d2d7e8bba6f373af0d6470f4b4bee3a
jj1here <jake.janovec@gmail.com>
2021-06-06 18:27:08 -0500

    made the code into a function

--- pass.py
+++ pass.py
@@ -1,19 +1,23 @@
 import re
 
+def paswrdCheck(text) :
+
+    upperRegex = re.compile(r'[A-Z]')
+    lowerRegex = re.compile(r'[a-z]')
+    digitRegex = re.compile(r'[0-9]')
+
+    upper = upperRegex.search(text)
+    lower = lowerRegex.search(text)
+    digit = digitRegex.search(text)
+
+    if len(text) > 8 and upper != None and lower != None and digit != None :
+            print("Strong Password!")
+    else:
+        print("Weak Password!")
+
 text= "Abc12dasa!"
 
-upperRegex = re.compile(r'[A-Z]')
-lowerRegex = re.compile(r'[a-z]')
-digitRegex = re.compile(r'[0-9]')
-
-upper = upperRegex.search(text)
-lower = lowerRegex.search(text)
-digit = digitRegex.search(text)
-
-if len(text) > 8 and upper != None and lower != None and digit != None :
-        print("Strong Password!")
-else:
-    print("Weak Password!")
+paswrdCheck(text)
 
 
 
